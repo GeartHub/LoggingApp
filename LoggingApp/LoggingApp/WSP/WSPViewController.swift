89d2b759588200936cf69d2057b01ccc172fb458
Josekappert1993 <jose_kappert1993@hotmail.com>
2020-01-06 15:20:04 +0100

    Added design to storyboard

--- LoggingApp/LoggingApp/WSP/WSPViewController.swift
+++ LoggingApp/LoggingApp/WSP/WSPViewController.swift
@@ -20,6 +20,7 @@ class WSPViewController: UIViewController {
     var logbookItem: FormMO?
     let context = CoreDataStack.instance.managedObjectContext
     var isNewLogbookItem: Bool = true
+    @IBOutlet weak var wspTitleLabel: UILabel!
     
     private lazy var questionsView : UITableView = {
         var tableView = UITableView()
@@ -102,9 +103,9 @@ class WSPViewController: UIViewController {
     }
     
     internal func setupConstraints() {
-        NSLayoutConstraint.activate([questionsView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-                                     questionsView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-                                     questionsView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+        NSLayoutConstraint.activate([questionsView.topAnchor.constraint(equalTo: wspTitleLabel.bottomAnchor),
+                                     questionsView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40),
+                                     questionsView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40),
                                      questionsView.bottomAnchor.constraint(equalTo: backAndForwardBar.topAnchor)])
         
         NSLayoutConstraint.activate([backAndForwardBar.topAnchor.constraint(equalTo: questionsView.bottomAnchor),
